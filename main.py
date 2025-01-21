b0bad26e1e26257b8540eac1b515fecf9553ae9d
Olha <1helga.starlight1@gmail.com>
2025-01-21 15:39:50 +0100

    first code commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import string
+def get_letter_range(input_str):
+    letters = string.ascii_letters
+    parts = input_str.split('-')
+    start = parts[0]
+    end = parts[1]
+    start_index = letters.index(start)
+    end_index = letters.index(end)
+    result = letters[start_index:end_index+1]
+    return result
+user_input = input("Введіть дві літери через дефіс (наприклад, a-c): ")
+output = get_letter_range(user_input)
+print(user_input + " -> " + output)
